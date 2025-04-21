c381c1c1c43cae828abdc91e8541e20d70f375c7
Urs Baumann <github@m.ubaumann.ch>
2025-04-21 19:05:12 +0000

    Add first try mcp server app

--- src/mkdocs_mcp/__main__.py
+++ src/mkdocs_mcp/__main__.py
@@ -3,8 +3,27 @@ import logging
 from mkdocs.commands import build
 from mkdocs import config
 
+from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp.resources import FunctionResource
+from pydantic.networks import AnyUrl
+
+from mkdocs_mcp.plugin import PageInfo
+
 
 log = logging.getLogger(__name__)
+mcp = FastMCP()
+
+
+def add_resource(mcp: FastMCP, page: PageInfo) -> None:
+    # Register as regular resource
+    resource = FunctionResource(
+        uri=AnyUrl(f"docs://{page.title}"),
+        name=page.title,
+        description=f"The {page.title} documentation",
+        mime_type="text/markdown",
+        fn=lambda: page.content,
+    )
+    mcp.add_resource(resource=resource)
 
 
 def main(**kwargs):
@@ -17,10 +36,13 @@ def main(**kwargs):
     finally:
         cfg.plugins.on_shutdown()
 
-    from rich import inspect
+    mcp_plugin = cfg.plugins.get("mcp")
 
-    inspect(cfg.plugins)
-    inspect(cfg.plugins.get("mcp"))
+    mcp.name = cfg.site_name
+    for _, page in mcp_plugin.md_pages.items():
+        add_resource(mcp, page)
+
+    mcp.run()
 
 
 if __name__ == "__main__":
