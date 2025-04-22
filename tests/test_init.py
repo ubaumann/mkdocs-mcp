5c54ed89f91f238b0079fe755e627faf0b0165f3
Urs Baumann <github@m.ubaumann.ch>
2025-04-22 18:43:49 +0000

    Add populate mcp app tests

--- tests/test_init.py
+++
@@ -1,5 +0,0 @@
-import mkdocs_mcp
-
-
-def test_init():
-    assert type(mkdocs_mcp).__name__ == "module"
---
+++ tests/test_populate_mcp_app.py
@@ -0,0 +1,41 @@
+import pytest
+from pathlib import Path
+from mcp.server.fastmcp import FastMCP
+
+
+from mkdocs_mcp.plugin import PageInfo
+from mcp.server.fastmcp.resources import Resource
+from mkdocs_mcp.__main__ import add_resource
+
+
+@pytest.fixture
+def mcp_app() -> FastMCP:
+    return FastMCP()
+
+
+@pytest.fixture
+def pages() -> list[PageInfo]:
+    return [
+        PageInfo(
+            title="index", path_md=Path("./index.md"), content="# Title\n\nmore text"
+        ),
+        PageInfo(
+            title="about", path_md=Path("./about.md"), content="# About\n\nmore text"
+        ),
+    ]
+
+
+@pytest.fixture
+def mcp_resources(mcp_app, pages) -> list[Resource]:
+    add_resource(mcp_app, pages[0])
+    add_resource(mcp_app, pages[1])
+    return list(mcp_app._resource_manager._resources.values())
+
+
+def test_add_resource_len(mcp_resources) -> None:
+    assert len(mcp_resources) == 2
+
+
+@pytest.mark.parametrize("i", [0, 1])
+def test_add_resource_title(mcp_resources, pages, i) -> None:
+    assert mcp_resources[i].name == pages[i].title
